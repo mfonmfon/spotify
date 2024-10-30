1c58c14ed7ed07eb8c8ed553f3830ddb7a91de53
mfonmfon <mfonm579@gmail.com>
2024-10-30 10:58:57 +0100

    first commit for the spotify add

---
+++ src/test/java/com/spotifymusic/africa/nija/com/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifymusic.africa.nija.com.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
